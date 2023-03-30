86d3bbe46be8fc92b6b1637d0d7e92e766ad8a6a
VaishnaviDusane23 <129436447+VaishnaviDusane23@users.noreply.github.com>
2023-03-31 00:54:08 +0530

    Add files via upload

---
+++ Task2/script1.js
@@ -0,0 +1,11 @@
+const element = document.getElementById("button");
+element.addEventListener("click",wikipedia);
+
+
+
+
+ function wikipedia(){
+    console.warn("you are rendering to wikipedia to search a content");
+    
+
+}
